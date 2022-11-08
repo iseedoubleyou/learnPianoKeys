5318f82c245416e265e6ca0cb81db83ffad45ba2
iseedoubleyou <christian.wilhelmi@awk.ch>
2022-11-08 22:18:06 +0100

    changed title to h1-tag

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import NotenQuiz from "./components/NotenQuiz";
 
 function App() {
   return (
-    <div id="App" className="App" style={{fontSize: "32px", fontWeight: "bold"}}>
-      Notenquiz
+    <div id="App" className="App" style={{fontSize: "14px", fontWeight: "bold"}}>
+      <h1>Notenquiz</h1>
       <NotenQuiz />
     </div>
   );
